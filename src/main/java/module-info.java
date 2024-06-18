8ac21ba0209af60ac26685eaf9d1b33e7664cf9d
meowlet <ngoclink13@gmail.com>
2024-06-19 00:41:12 +0700

    Feature: Choose an entry point

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,12 @@ module org.example.schoolequipment {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
+    requires java.net.http;
+    requires com.google.gson;
 
     opens org.example.schoolequipment to javafx.fxml;
     exports org.example.schoolequipment;
+    exports org.example.schoolequipment.presentation.auth.sign_in;
+    exports org.example.schoolequipment.presentation.auth.sign_up;
+    opens org.example.schoolequipment.presentation.auth.sign_up to javafx.fxml;
 }
--- src/main/java/org/example/schoolequipment/Application.java
+++ src/main/java/org/example/schoolequipment/Application.java
@@ -1,20 +1,15 @@
 package org.example.schoolequipment;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.schoolequipment.presentation.auth.sign_up.SignUpScreen;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        SignUpScreen signUpScreen = new SignUpScreen();
+        signUpScreen.start(stage);
     }
 
     public static void main(String[] args) {
