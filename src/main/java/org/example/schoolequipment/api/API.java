ddf6cdea8ed30af73d6116102e77884548513b0a
meowlet <ngoclink13@gmail.com>
2024-07-02 14:12:19 +0700

    Feature: Add the update method to the api

--- src/main/java/org/example/schoolequipment/api/API.java
+++ src/main/java/org/example/schoolequipment/api/API.java
@@ -3,22 +3,36 @@ package org.example.schoolequipment.api;
 import org.example.schoolequipment.util.Constant;
 import org.example.schoolequipment.util.HttpRequestHelper;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 public class API {
+
+    Map<String, String> headers = new HashMap<>();
+
+    public API() {
+        try {
+            String token = new String(Files.readAllBytes(Paths.get(Constant.CREDENTIALS_FILE)));
+            headers.put("Authorization", "Bearer " + token);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public HttpRequestHelper.HttpResponse signUp(String userName, String password, String fullName, String email) {
         String requestUrl = Constant.API_ENDPOINT + "/auth/signup";
 
-        String requestBody = String.format("{\n" +
-                "    \"userName\": \"%s\",\n" +
-                "    \"password\": \"%s\",\n" +
-                "    \"fullName\": \"%s\",\n" +
-                "    \"email\": \"%s\"\n" +
-                "}", userName, password, fullName, email);
+        String requestBody = String.format("""
+                {
+                    "userName": "%s",
+                    "password": "%s",
+                    "fullName": "%s",
+                    "email": "%s"
+                }""", userName, password, fullName, email);
 
-        HttpRequestHelper.HttpResponse response = HttpRequestHelper.sendPostRequest(requestUrl, requestBody, null);
-        return response;
+        return HttpRequestHelper.sendPostRequest(requestUrl, requestBody, null);
     }
 
     public HttpRequestHelper.HttpResponse signIn(String userName, String password) {
@@ -29,17 +43,49 @@ public class API {
                 "    \"password\": \"%s\"\n" +
                 "}", userName, password);
 
-        HttpRequestHelper.HttpResponse response = HttpRequestHelper.sendPostRequest(requestUrl, requestBody, null);
-        return response;
+        return HttpRequestHelper.sendPostRequest(requestUrl, requestBody, null);
     }
 
-    public HttpRequestHelper.HttpResponse fetchEquipments() {
+    public HttpRequestHelper.HttpResponse fetchTypes() {
+        String requestUrl = Constant.API_ENDPOINT + "/type";
+        HttpRequestHelper.HttpResponse response = HttpRequestHelper.sendGetRequest(requestUrl, headers);
+        return HttpRequestHelper.sendGetRequest(requestUrl, headers);
+    }
+
+    public HttpRequestHelper.HttpResponse fetchLocations() {
+        String requestUrl = Constant.API_ENDPOINT + "/location";
+
+        return HttpRequestHelper.sendGetRequest(requestUrl, headers);
+    }
+
+    public HttpRequestHelper.HttpResponse fetchSuppliers() {
+        String requestUrl = Constant.API_ENDPOINT + "/supplier";
+
+        return HttpRequestHelper.sendGetRequest(requestUrl, headers);
+    }
+
+    public HttpRequestHelper.HttpResponse fetchEquipments(String query) {
+        String requestUrl = Constant.API_ENDPOINT + "/equipment?" + query;
+
+        return HttpRequestHelper.sendGetRequest(requestUrl, headers);
+    }
+
+    public HttpRequestHelper.HttpResponse updateEquipment(Map<String, String> updateData) {
         String requestUrl = Constant.API_ENDPOINT + "/equipment";
 
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI2NjU0Y2Q4MjBiOGYyZDM1NzU0NmMyNWQifQ.I-yZuYke3rp7mCmtTvrjie55x-3ToEfrkPEeyCCcw3k");
+        String requestBody = String.format("""
+                {
+                    "equipmentId": "%s",
+                    "name": "%s",
+                    "description": "%s",
+                    "type": "%s",
+                    "status": "%s",
+                    "location": "%s",
+                    "supplier": "%s",
+                    "price": %s
+                }""", updateData.get("equipmentId"), updateData.get("name"), updateData.get("description"), updateData.get("type"), updateData.get("status"), updateData.get("location"), updateData.get("supplier"), updateData.get("price"));
 
-        HttpRequestHelper.HttpResponse response = HttpRequestHelper.sendGetRequest(requestUrl, headers);
-        return response;
+        System.out.println(requestBody);
+        return HttpRequestHelper.sendPatchRequest(requestUrl, requestBody, headers);
     }
 }
