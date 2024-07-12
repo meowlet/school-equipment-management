7765a5637d396c5af4e3467647867940a50b2502
meowlet <ngoclink13@gmail.com>
2024-07-12 21:59:07 +0700

    Chore: Remove the default limit

--- src/main/java/org/example/schoolequipment/api/API.java
+++ src/main/java/org/example/schoolequipment/api/API.java
@@ -67,6 +67,9 @@ public class API {
     public HttpRequestHelper.HttpResponse fetchEquipments(String query) {
         String requestUrl = Constant.API_ENDPOINT + "/equipment?" + query;
 
+        System.out.println(HttpRequestHelper.sendGetRequest(requestUrl, headers).getBody());
+        System.out.println(requestUrl);
+
         return HttpRequestHelper.sendGetRequest(requestUrl, headers);
     }
 
@@ -85,7 +88,34 @@ public class API {
                     "price": %s
                 }""", updateData.get("equipmentId"), updateData.get("name"), updateData.get("description"), updateData.get("type"), updateData.get("status"), updateData.get("location"), updateData.get("supplier"), updateData.get("price"));
 
-        System.out.println(requestBody);
         return HttpRequestHelper.sendPatchRequest(requestUrl, requestBody, headers);
+    }
+
+    public HttpRequestHelper.HttpResponse addEquipment(Map<String, String> equipmentData) {
+        String requestUrl = Constant.API_ENDPOINT + "/equipment" ;
+
+
+        String requestBody = String.format("""
+                {
+                    "name": "%s",
+                    "description": "%s",
+                    "type": "%s",
+                    "status": "%s",
+                    "location": "%s",
+                    "supplier": "%s",
+                    "price": %s
+                }""", equipmentData.get("name"), equipmentData.get("description"), equipmentData.get("type"), equipmentData.get("status"), equipmentData.get("location"), equipmentData.get("supplier"), equipmentData.get("price"));
+
+
+        System.out.println(requestBody);
+        System.out.println(requestUrl);
+        System.out.println(headers);
+        return HttpRequestHelper.sendPostRequest(requestUrl, requestBody, headers);
+    }
+
+    public HttpRequestHelper.HttpResponse deleteEquipment(String equipmentId) {
+        String requestUrl = Constant.API_ENDPOINT + "/equipment/" + equipmentId;
+
+        return HttpRequestHelper.sendDeleteRequest(requestUrl, headers);
     }
 }
