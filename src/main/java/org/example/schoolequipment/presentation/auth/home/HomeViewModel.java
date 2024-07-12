299af6d066ef7a183210d19ef5e8f6b80590eb10
meowlet <ngoclink13@gmail.com>
2024-07-12 22:00:04 +0700

    Refactor: Port everything state-related to the State

--- src/main/java/org/example/schoolequipment/presentation/auth/home/HomeViewModel.java
+++ src/main/java/org/example/schoolequipment/presentation/auth/home/HomeViewModel.java
@@ -14,44 +14,76 @@ import org.example.schoolequipment.presentation.auth.sign_in.SignInScreen;
 import org.example.schoolequipment.util.HttpRequestHelper;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HomeViewModel {
-    public HomeState state;
+    private final HomeState state;
+    private final Stage stage;
+    private final API api;
 
-    private Stage stage;
+    public void setClearingFilters(boolean clearingFilters) {
+        isClearingFilters = clearingFilters;
+    }
 
-    public boolean isClearingFilters = false;
-
+    private boolean isClearingFilters = false;
 
     public HomeViewModel(Stage stage) {
         this.stage = stage;
-        state = new HomeState();
+        this.state = new HomeState();
+        this.api = new API();
+
+        // Set up listeners for reactive updates
+        state.queryProperty().addListener((observable, oldValue, newValue) -> fetchEquipments());
+        state.filtersProperty().addListener((observable, oldValue, newValue) -> fetchEquipments());
     }
 
-    public void responseHandler(HttpRequestHelper.HttpResponse response) {
+    public HomeState getState() {
+        return state;
+    }
+
+    private void responseHandler(HttpRequestHelper.HttpResponse response) {
         if (response.getStatusCode() == 401) {
             Platform.runLater(this::navigateToSignIn);
         }
     }
 
-    public void navigateToSignIn() {
+    private void navigateToSignIn() {
         new SignInScreen().start(stage);
     }
 
-    public void fetchEquipments(String query) {
-        System.out.println(isClearingFilters);
+    public void fetchEquipments() {
+
         if (isClearingFilters) {
             return;
         }
-        Task<Void> fetchTask = new Task<Void>() {
+
+        state.setLoading(true);
+
+        Task<Void> fetchTask = new Task<>() {
             @Override
             protected Void call() throws Exception {
-                API api = new API();
+                String query = buildQuery(true);
                 HttpRequestHelper.HttpResponse equipments = api.fetchEquipments(query);
 
                 responseHandler(equipments);
+
+                HttpRequestHelper.HttpResponse fullEquipments = api.fetchEquipments(buildQuery(false));
+                if (fullEquipments.getStatusCode() == 200) {
+                    Gson gson = new Gson();
+                    Type equipmentListType = new TypeToken<List<Equipment>>() {
+                    }.getType();
+                    List<Equipment> equipmentList = gson.fromJson(fullEquipments.getBody(), equipmentListType);
+                    Platform.runLater(() -> {
+                        state.setTotalItems(equipmentList.size());
+                    });
+                } else {
+                    Platform.runLater(() -> {
+                        state.setError(equipments.getBody());
+                        state.setLoading(false);
+                    });
+                }
 
                 if (equipments.getStatusCode() == 200) {
                     Gson gson = new Gson();
@@ -59,15 +91,14 @@ public class HomeViewModel {
                     }.getType();
                     List<Equipment> equipmentList = gson.fromJson(equipments.getBody(), equipmentListType);
 
-                    // Update the state on the JavaFX Application Thread
-                    javafx.application.Platform.runLater(() -> {
-                        state.equipments.clear();
-                        state.equipments.addAll(equipmentList);
+                    Platform.runLater(() -> {
+                        state.getEquipments().clear();
+                        state.getEquipments().addAll(equipmentList);
+                        state.setLoading(false);
                     });
                 } else {
-                    javafx.application.Platform.runLater(() -> {
-                        state.error.set(equipments.getBody());
-                    });
+                    Platform.runLater(() -> state.setError(equipments.getBody()));
+                    state.setLoading(false);
                 }
 
                 return null;
@@ -75,7 +106,6 @@ public class HomeViewModel {
         };
 
         fetchTask.setOnFailed(e -> {
-            // Handle any exceptions here
             Throwable exception = e.getSource().getException();
             exception.printStackTrace();
         });
@@ -83,26 +113,61 @@ public class HomeViewModel {
         new Thread(fetchTask).start();
     }
 
-    //clear states
+    private String buildQuery(boolean includeLimit) {
+        StringBuilder query = new StringBuilder();
+
+        if (!state.getQuery().isEmpty()) {
+            query.append("query=").append(state.getQuery().replace(" ", "+"));
+        }
+
+        boolean limitSet = false;
+
+        // Iterate over filters, ignoring the limit filter if found
+        for (Map.Entry<String, String> filter : state.getFilters().entrySet()) {
+            if ("limit".equals(filter.getKey()) && !includeLimit) {
+                continue; // Skip adding the limit filter to the query
+            }
+            if (!filter.getValue().isEmpty()) {
+                if (!query.isEmpty()) {
+                    query.append("&");
+                }
+                query.append(filter.getKey()).append("=").append(filter.getValue());
+                if ("limit".equals(filter.getKey())) {
+                    limitSet = true;
+                }
+            }
+        }
+
+        if (!limitSet && includeLimit) {
+            if (!query.isEmpty()) {
+                query.append("&");
+            }
+            query.append("limit=5");
+        }
+
+
+        return query.toString();
+    }
+
     public void clearFilters() {
-        this.isClearingFilters = true;
-        state.filters.clear();
-        fetchEquipments("");
+        state.setFilters(new HashMap<>());
+        state.setQuery("");
+        isClearingFilters = false;
+        fetchEquipments();
     }
 
     public void fetchAll() {
-        Task<Void> fetchTask = new Task<Void>() {
-            API api = new API();
+        Task<Void> fetchTask = new Task<>() {
             @Override
             protected Void call() throws Exception {
-                fetchEquipments("");
+                fetchEquipments();
                 HttpRequestHelper.HttpResponse equipmentTypes = api.fetchTypes();
                 HttpRequestHelper.HttpResponse suppliers = api.fetchSuppliers();
                 HttpRequestHelper.HttpResponse locations = api.fetchLocations();
 
                 responseHandler(equipmentTypes);
 
-                if (locations.getStatusCode() == 200 || equipmentTypes.getStatusCode() == 200 || suppliers.getStatusCode() == 200) {
+                if (locations.getStatusCode() == 200 && equipmentTypes.getStatusCode() == 200 && suppliers.getStatusCode() == 200) {
                     Gson gson = new Gson();
                     Type equipmentTypeListType = new TypeToken<List<EquipmentType>>() {
                     }.getType();
@@ -114,19 +179,16 @@ public class HomeViewModel {
                     List<Supplier> supplierList = gson.fromJson(suppliers.getBody(), supplierListType);
                     List<Location> locationList = gson.fromJson(locations.getBody(), locationListType);
 
-                    // Update the state on the JavaFX Application Thread
-                    javafx.application.Platform.runLater(() -> {
-                        state.equipmentTypes.clear();
-                        state.equipmentTypes.addAll(equipmentTypesList);
-                        state.suppliers.clear();
-                        state.suppliers.addAll(supplierList);
-                        state.locations.clear();
-                        state.locations.addAll(locationList);
+                    Platform.runLater(() -> {
+                        state.getEquipmentTypes().clear();
+                        state.getEquipmentTypes().addAll(equipmentTypesList);
+                        state.getSuppliers().clear();
+                        state.getSuppliers().addAll(supplierList);
+                        state.getLocations().clear();
+                        state.getLocations().addAll(locationList);
                     });
                 } else {
-                    javafx.application.Platform.runLater(() -> {
-                        state.error.set(equipmentTypes.getBody());
-                    });
+                    Platform.runLater(() -> state.setError("Failed to fetch all data"));
                 }
 
                 return null;
@@ -134,7 +196,6 @@ public class HomeViewModel {
         };
 
         fetchTask.setOnFailed(e -> {
-            // Handle any exceptions here
             Throwable exception = e.getSource().getException();
             exception.printStackTrace();
         });
@@ -142,33 +203,63 @@ public class HomeViewModel {
         new Thread(fetchTask).start();
     }
 
-
-    public void updateQuery() {
-        StringBuilder query = new StringBuilder();
-
-        // Add the searchTerm to the query if it's not null or empty
-        if (state.query != null && !state.query.isEmpty()) {
-            String queryWithPlus = state.query.replace(" ", "+");
-            query.append("query=").append(queryWithPlus);
-        }
-
-        // Add the filters to the query
-        for (Map.Entry<String, String> filter : state.filters.entrySet()) {
-            // Check if the filter value is not null or empty
-            if (filter.getValue() != null && !filter.getValue().isEmpty()) {
-                // If the query is not empty, add an '&' before the filter
-                if (query.length() > 0) {
-                    query.append("&");
-                }
-                query.append(filter.getKey()).append("=").append(filter.getValue());
+    public void updateEquipment() {
+        Task<Void> updateTask = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                HttpRequestHelper.HttpResponse response = api.updateEquipment(state.getUpdateData());
+                Platform.runLater(() -> {
+                    if (response.getStatusCode() == 200) {
+                        fetchEquipments();
+                        state.setFormError("Equipment updated successfully");
+                    } else {
+                        state.setFormError(response.getBody());
+                    }
+                });
+                return null;
             }
-        }
+        };
 
-        fetchEquipments(query.toString());
+        new Thread(updateTask).start();
     }
 
-    public void updateEquipment() {
-        API api = new API();
-        api.updateEquipment(state.updateData);
+    public void createEquipment() {
+        Task<Void> createTask = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                HttpRequestHelper.HttpResponse response = api.addEquipment(state.getEquipmentData());
+                Platform.runLater(() -> {
+                    if (response.getStatusCode() == 200) {
+                        fetchEquipments();
+                        state.setFormError("Equipment added successfully");
+                    } else {
+                        state.setFormError(response.getBody());
+                    }
+                });
+                return null;
+            }
+        };
+
+        new Thread(createTask).start();
+    }
+
+    public void deleteEquipment(String equipmentId) {
+        Task<Void> deleteTask = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                HttpRequestHelper.HttpResponse response = api.deleteEquipment(equipmentId);
+                Platform.runLater(() -> {
+                    if (response.getStatusCode() == 200) {
+                        fetchEquipments();
+                        state.setFormError("Equipment deleted successfully");
+                    } else {
+                        state.setFormError("Failed to delete equipment: " + response.getBody());
+                    }
+                });
+                return null;
+            }
+        };
+
+        new Thread(deleteTask).start();
     }
 }
